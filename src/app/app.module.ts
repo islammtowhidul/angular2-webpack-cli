c5c521f9dc0c9ce6125b16eb118166d6e1a7392e
Md Towhidul Islam <towhid087@gmail.com>
2016-10-25 11:31:40 -0400

    all files

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { MyButtonComponent } from './my-button/my-button.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyButtonComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
